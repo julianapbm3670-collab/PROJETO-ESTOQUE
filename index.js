5e722894c06ef5b884bd3e7b2ed5fabb6e9b3d78
julianapbm3670-collab <julianapbm3670@gmail.com>
2026-04-30 00:39:33 +0000

    nova modificaçao

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+import express from "express"
+import cors from "cors"
+import router from "./src/routes/usuarios.routes.js"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/usuarios", router);
+
+const PORTA = 3000;
+app.listen(PORTA, () => {
+    console.log('API rodando em htpp://localhost:${PORTA}');
+});
+
+
+
